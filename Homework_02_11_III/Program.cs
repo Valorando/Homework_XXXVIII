a6ea13742f5a6f74b7e2f77a8f8c561a3d4dd376
Valorando <vadimsokil.dnipro@gmail.com>
2023-11-02 16:58:54 +0200

    Task 3 completed.

---
+++ Homework_02_11_III/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework_02_11_III
+{
+    internal class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            Random random = new Random();
+            int delaySeconds = random.Next(5, 31);
+            Console.WriteLine($"Приготовьтесь!");
+            await Task.Delay(delaySeconds * 1000);
+
+            Console.WriteLine("Нажмите на любую клавишу!");
+            DateTime startTime = DateTime.Now;
+            bool keyPressed = false;
+
+            var keyTask = Task.Run(() => Console.ReadKey(intercept: true));
+
+            while (!keyPressed && !keyTask.IsCompleted)
+            {
+                if (Console.KeyAvailable)
+                {
+                    keyTask.Wait();
+                    keyPressed = true;
+                }
+            }
+
+            if (keyPressed = true)
+            {
+                DateTime endTime = DateTime.Now;
+                TimeSpan duration = endTime - startTime;
+                Console.WriteLine($"Вам потребовалось {duration.TotalSeconds:F2} секунд для нажатия клавиши.");
+                Console.Read();
+            }
+        }
+    }
+}
